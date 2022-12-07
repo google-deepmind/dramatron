5018ae8bb915ba2749253b70937eeecab7507c3d
jlove29 <juliettelove@google.com>
2022-12-07 23:42:26 +0000

    Change UI prompts to sci-fi

--- docs/prefixes.js
+++ docs/prefixes.js
@@ -35,165 +35,183 @@ export const PREVIOUS_ELEMENT = 'Previous beat: ';
 export const BEAT_ELEMENT = 'Beat: ';
 
 export const CHARACTERS_PROMPT = `
-Example 1. In the following story, Bob has an argument with his best friend, Charles.
-${CHARACTER_MARKER}Bob ${
-    DESCRIPTION_MARKER}Bob is mid forties, corpulent. He is pompous and self-absorbed. Bob is married to Alice.${
-    STOP_MARKER}
-${CHARACTER_MARKER}Alice ${
-    DESCRIPTION_MARKER}Bob's wife Alice is thirty something, serious and bookish.${
-    STOP_MARKER}
-${CHARACTER_MARKER}Charles ${
-    DESCRIPTION_MARKER}Charles is forty something and athletic. Charles and Bob are best friends.${
-    STOP_MARKER}
+Example 1. A science-fiction fantasy about a naive but ambitious farm boy from a backwater desert who discovers powers he never knew he had when he teams up with a feisty princess, a mercenary space pilot and an old wizard warrior to lead a ragtag rebellion against the sinister forces of the evil Galactic Empire.
+${CHARACTER_MARKER}Luke Skywalker ${DESCRIPTION_MARKER}Luke Skywalker is the hero. A naive farm boy, he will discover special powers under the guidance of mentor Ben Kenobi.${STOP_MARKER}
+${CHARACTER_MARKER}Ben Kenobi ${DESCRIPTION_MARKER}Ben Kenobi is the mentor figure. A recluse Jedi warrior, he will take Luke Skywalker as apprentice.${STOP_MARKER}
+${CHARACTER_MARKER}Darth Vader ${DESCRIPTION_MARKER}Darth Vader is the antagonist. As a commander of the evil Galactic Empire, he controls space station The Death Star.${STOP_MARKER}
+${CHARACTER_MARKER}Princess Leia ${DESCRIPTION_MARKER}Princess Leia is a feisty and brave leader of the Rebellion. She holds the plans of the Death Star. She will become Luke's friend.${STOP_MARKER}
+${CHARACTER_MARKER}Han Solo ${DESCRIPTION_MARKER}Han Solo is a brash mercenary space pilot of the Millenium Falcon and a friend of Chebacca. He will take Luke on his spaceship.${STOP_MARKER}
+${CHARACTER_MARKER}Chewbacca ${DESCRIPTION_MARKER}Chewbacca is a furry and trustful monster. He is a friend of Han Solo and a copilot on the Millemium Falcon.${STOP_MARKER}
 ${END_MARKER}
-Example 2. In the following story, Terence tries and fails to become a wizard.
-${CHARACTER_MARKER}Terence ${
-    DESCRIPTION_MARKER}Terence is twenty one, small, dark. He is eager to learn, and ambitious. Terence is the student of Pompulus.${
-    STOP_MARKER}
-${CHARACTER_MARKER}Pompulus the Wizard ${
-    DESCRIPTION_MARKER}Pompulus the Wizard is ancient, white haired, emaciated. He is pompous and expansive. Pompulus instructs Terence in wizardry.${
-    STOP_MARKER}
-${CHARACTER_MARKER}Brenda ${
-    DESCRIPTION_MARKER}Brenda is the mother of Terence. She is intelligent and caring.${
-    STOP_MARKER}
-${END_MARKER}
-Example 3. In the following story, Jerry falls in love with Lydia.
-${CHARACTER_MARKER}Jerry ${
-    DESCRIPTION_MARKER}Jerry is in his 30s, sallow, a bit puffy. His hair is a little messy, his suit is either vintage or just old and dirty and sort of threadbare. His bright tie has a photograph of a rodeo printed on it. Jerry lives on the same block as Lydia.${
-    STOP_MARKER}
-${CHARACTER_MARKER}Lydia ${
-    DESCRIPTION_MARKER}Lydia is wearing a fluorescent orange hooded sweatshirt. She's in her early thirties. Lydia is Jerry's neighbor.${
-    STOP_MARKER}
-${END_MARKER}
-Example 4. In the following story, `;
+Example 2. `
 
 export const SCENE_PROMPT = `
-Example 1. In the following story, Bob has an argument with his best friend, Charles.
-Bob is mid forties, corpulent. He is pompous and self-absorbed. Bob is married to Alice.
-Bob's wife Alice is thirty something, serious and bookish.
-Charles is forty something and athletic. Charles and Bob are best friends.
+Example 1. A science-fiction fantasy about a naive but ambitious farm boy from a backwater desert who discovers powers he never knew he had when he teams up with a feisty princess, a mercenary space pilot and an old wizard warrior to lead a ragtag rebellion against the sinister forces of the evil Galactic Empire.
+Luke Skywalker is the hero. A naive farm boy, he will discover special powers under the guidance of mentor Ben Kenobi.
+Ben Kenobi is the mentor figure. A recluse Jedi warrior, he will take Luke Skywalker as apprentice.
+Darth Vader is the antagonist. As a commander of the evil Galactic Empire, he controls space station The Death Star.
+Princess Leia holds the plans of the Death Star. She is feisty and brave. She will become Luke's friend.
+Han Solo is a brash mercenary space pilot of the Millenium Falcon and a friend of Chebacca. He will take Luke on his spaceship.
+Chewbacca is a furry and trustful monster. He is a friend of Han Solo and a copilot on the Millemium Falcon.
 
 ${SCENES_MARKER}
 
-${PLACE_ELEMENT}The pub.
-${PLOT_ELEMENT}Beginning.
-${BEAT_ELEMENT}Alice, Bob, and Charles are sitting at a table in the corner and make a number of jokes, but Alice laughs loudest at Charles' jokes. Bob gets grumpy that Alice is paying Charles more attention.
+${PLACE_ELEMENT}A farm on planet Tatooine.
+${PLOT_ELEMENT}The Ordinary World.
+Beat: Luke Skywalker is living a normal and humble life as a farm boy on his home planet.
 
-${PLACE_ELEMENT}The office.
-${PLOT_ELEMENT}Middle.
-${BEAT_ELEMENT}Charles, sitting at his desk, mocks Bob for various perceived flaws. Charles insults Bob once too often, and Bob is visibly offended.
+${PLACE_ELEMENT}Desert of Tatooine.
+${PLOT_ELEMENT}Tall to Adventure.
+Beat: Luke is called to his adventure by robot R2-D2 and Ben Kenobi. Luke triggers R2-D2’s message from Princess Leia and is intrigued by her message. When R2-D2 escapes to find Ben Kenobi, Luke follows and is later saved by Kenobi, who goes on to tell Luke about his Jedi heritage. Kenobi suggests that he should come with him.
 
-${PLACE_ELEMENT}Charles' flat.
-${PLOT_ELEMENT}Conclusion.
-${BEAT_ELEMENT}Charles tries to apologise, but Bob is unmoved. Bob tells Charles he has gone too far this time, and storms out.
+${PLACE_ELEMENT}Ben Kenobi's farm.
+${PLOT_ELEMENT}Tefusal of the Call.
+Beat: Luke refuses Kenobi, telling him that he can take Kenobi and the droids as far as Mos Eisley Spaceport — but he can’t possibly leave his Aunt and Uncle behind for some space adventure.
 
-${END_MARKER}
+${PLACE_ELEMENT}On spaceship The Millennium Falcon.
+${PLOT_ELEMENT}Tests, Allies, and Enemies.
+Beat: After Luke, Kenobi, and the droids hire Han Solo and Chewbacca to transport them onto Alderaan, Kenobi begins Luke’s training in the ways of the Force. Wielding his father’s lightsaber, Kenobi challenges Luke. At first, he can’t do it. But then Kenobi Kenobi Luke him to reach out and trust his feelings. Luke succeeds.
 
-Example 2. In the following story, Terence tries and fails to become a wizard. The main characters are Terence, Pompulus (a wizard), and Brenda (Terence’s mother).
-Terence is twenty one, small, dark. He is eager to learn, and ambitious. Terence is the student of Pompulus.
-Pompulus the Wizard is ancient, white haired, emaciated. He is pompous and expansive. Pompulus instructs Terence in wizardry.
-Brenda is the mother of Terence. She is intelligent and caring.
+${PLACE_ELEMENT}On space station The Death Star.
+${PLOT_ELEMENT}The Ordeal.
+Beat: As Kenobi goes off to deactivate the tractor beam so they can escape, Luke, Han, and Chewbacca discover that Princess Leia is being held on the Death Star with them. They rescue her and escape to the Millennium Falcon, hoping that Kenobi has successfully deactivated the tractor beam. Kenobi later sacrifices himself as Luke watches Darth Vader strike him down. Luke must now avenge his fallen mentor and carry on his teachings.
 
-${SCENES_MARKER}
+${PLACE_ELEMENT}On fighter ship X-Wing.
+${PLOT_ELEMENT}The Resurrection.
+Beat: The Rebels — along with Luke as an X-Wing pilot — take on the Death Star. The Rebellion and the Galactic Empire wage war in an epic space battle. Luke is the only X-Wing pilot that was able to get within the trenches of the Death Star. But Darth Vader and his wingmen are in hot pursuit. Just as Darth Vader is about to destroy Luke, Han returns and clears the way for Luke. Luke uses the Force to guide his aiming as he fires upon the sole weak point of the deadly Death Star, destroying it for good.
 
-${PLACE_ELEMENT}Pompulus the Wizard's tower.
-${PLOT_ELEMENT}Beginning.
-${BEAT_ELEMENT}Pompulus asks Terence to do a number of tedious tasks, and Terence reluctantly performs them. Terence asks Pompulus if he can become a wizard one day; Pompulus says Terence needs to take the wizard's test.
-
-${PLACE_ELEMENT}Pompulus the Wizard's examination halls.
-${PLOT_ELEMENT}Middle.
-${BEAT_ELEMENT}Terence is writing furiously at his desk and expresses frustration that the questions are so difficult, but Pompulus is not sympathetic. Pompulus tells Terence that he has failed the test.
-
-${PLACE_ELEMENT}Terence's house.
-${PLOT_ELEMENT}Conclusion.
-${BEAT_ELEMENT}Lying on his bed, Terence wishes he had studied harder, and his mother Brenda tries to reassure him. Terence vows to study harder and try again next year.
+${PLACE_ELEMENT}At the Rebellion base.
+${PLOT_ELEMENT}The Return.
+Beat: Luke and Han return to the Rebellion base, triumphant, as they receive medals for the heroic journey. There is peace throughout the galaxy — at least for now.
 
 ${END_MARKER}
-
-Example 3. In the following story, `;
+Example 2. `;
 
 export const SETTING_PROMPT = `
-Example 1. In the following story, Terence tries and fails to become a wizard.
-${PLACE_ELEMENT}The train station.
-${DESCRIPTION_ELEMENT}It's grey. The platform is packed with business commuters: suits, overcoats. There is such a lack of colour it almost seems as if it's a black and white shot. The platform across the tracks is empty.${
-    END_MARKER}
-Example 2. In the following story, Jerry falls in love with Lydia.
-${PLACE_ELEMENT}The bar.
-${DESCRIPTION_ELEMENT}The bar is dirty, more than a little run down. As we pan across several empty tables, we can almost smell the odor of last night's beer and crushed pretzels on the floor.${
-    END_MARKER}
-Example 3. In the following story, Bob has an argument with his best friend, Charles.
-${PLACE_ELEMENT}The diner.
-${DESCRIPTION_ELEMENT}It's a local tourist place, but off-season empty.${
-    END_MARKER}
+Example 1. Morgan adopts a new cat, Misterio, who sets a curse on anyone that pets them.
+${PLACE_ELEMENT}The Adoption Center.
+${DESCRIPTION_ELEMENT}The Adoption Center is a sad place, especially for an unadopted pet. It is full of walls and walls of cages and cages. Inside of each is an abandoned animal, longing for a home. The lighting is dim, gray, buzzing fluorescent.${END_MARKER}
+Example 2. James finds a well in his backyard that is haunted by the ghost of Sam.
+${PLACE_ELEMENT}The well.
+${DESCRIPTION_ELEMENT}The well is buried under grass and hedges. It is at least twenty feet deep, if not more and it is masoned with stones. It is 150 years old at least. It stinks of stale, standing water, and has vines growing up the sides. It is narrow enough to not be able to fit down if you are a grown adult human.${END_MARKER}
+Example 3. Mr. Dorbenson finds a book at a garage sale that tells the story of his own life. And it ends in a murder!
+${PLACE_ELEMENT}The garage sale.
+${DESCRIPTION_ELEMENT}It is a garage packed with dusty household goods and antiques. There is a box at the back that says FREE and is full of paper back books.${END_MARKER}
 Example 4. `;
 
 export const TITLES_PROMPT = `
-Examples of alternative, original and descriptive titles known for play and film scripts.
+Examples of alternative, original and descriptive titles for known play and film scripts.
 
-Example 1. In the following story, Bob has an argument with his best friend, Charles. Title: The End of A Friend${
-    END_MARKER}
-Example 2. In the following story, Terence tries and fails to become a wizard. Title: Spellcaster${
-    END_MARKER}
-Example 3. In the following story, Tom falls in love with Daisy. Title: The Greatest Love Story Ever Told${
-    END_MARKER}
-Example 4. In the following story, `;
+Example 1. A science-fiction fantasy about a naive but ambitious farm boy from a backwater desert who discovers powers he never knew he had when he teams up with a feisty princess, a mercenary space pilot and an old wizard warrior to lead a ragtag rebellion against the sinister forces of the evil Galactic Empire. Title: The Death Star's Menace${END_MARKER}
+Example 2. Residents of San Fernando Valley are under attack by flying saucers from outer space. The aliens are extraterrestrials who seek to stop humanity from creating a doomsday weapon that could destroy the universe and unleash the living dead to stalk humans who wander into the cemetery looking for evidence of the UFOs. The hero Jeff, an airline pilot, will face the aliens. Title: The Day The Earth Was Saved By Outer Space.${END_MARKER}
+Example 3. `;
 
 export const DIALOG_PROMPT = `
 Example 1.
-${PLACE_ELEMENT}The bar.
-${DESCRIPTION_ELEMENT}The bar is dirty, more than a little run down. If there is ever a cook on duty, he's not here now. As we pan across several empty tables, we can almost smell the odor of last night's beer and crushed pretzels on the floor.
-${CHARACTERS_ELEMENT}Jerry is in his 30s, sallow, a bit puffy. His hair is a little messy, his suit is either vintage or just old and dirty and sort of threadbare. His bright tie has a photograph of a rodeo printed on it. Jerry lives on the same block as Lydia.
-Lydia is wearing a fluorescent orange hooded sweatshirt. She's in her early thirties. Lydia is Jerry's neighbor.
-${PLOT_ELEMENT}Middle.
-${SUMMARY_ELEMENT}Jerry falls in love with Lydia.
-${PREVIOUS_ELEMENT}Lydia notices Jerry sitting at the bar and makes small talk.
-${BEAT_ELEMENT}Jerry tells Lydia the story of his proposal. Jerry tells Lydia that he failed to propose.
+${PLACE_ELEMENT}Cockpit of an airplane.
+${DESCRIPTION_ELEMENT}Cockpit of a modern passenger airplane, American Flight 812.
+${CHARACTERS_ELEMENT}Jeff is the hero. A man in his early forties, he tries to stay calm in all circumstance. Jeff is now a airline pilot. Danny, a young airplane pilot in his thirties, is eager to learn but can quickly lose his composture. Danny is enamored of Edith. Edith, an experienced stewardess with a good sense of humour, is trustworthy and dependable. Edith likes to tease Danny.
+${PLOT_ELEMENT}Crossing the First Threshold.
+${SUMMARY_ELEMENT}Residents of San Fernando Valley are under attack by flying saucers from outer space. The aliens are extraterrestrials who seek to stop humanity from creating a doomsday weapon that could destroy the universe and unleash the living dead to stalk humans who wander into the cemetery looking for evidence of the UFOs. The hero Jeff, an airline pilot, will face the aliens.
+${PREVIOUS_ELEMENT}Flight captain Jeff reluctantly leaves his wife Paula to go for a two-day flight.
+${BEAT_ELEMENT}At the cockpit, flight captain Jeff is preoccupied by the flying saucer appearances and graveyard incidents in his home town, where he left wis wife Paula. Without success, co-pilot Danny and stewardess Edith try to reassure him.
 
 ${DIALOG_MARKER}
 
-LYDIA
-Tell me how you proposed.
+DANNY
+You're mighty silent this trip, Jeff.
 
-JERRY
-No no...
+JEFF
+Huh?
 
-LYDIA
-(impatient) Oh, just tell the story.
+DANNY
+You haven't spoken ten words since takeoff.
 
-Pause. Jerry is thinking.
+JEFF
+I guess I'm preoccupied, Danny.
 
-JERRY
-So our first date, she told me about her favorite place in the world, the seven pools of Hana on the island of Maui...
+DANNY
+We've got thirty-three passengers back there that have time to be preoccupied.
+Flying this flybird doesn't give you that opportunity.
 
-LYDIA
-Gorgeous.
+JEFF
+I guess you're right, Danny.
 
-JERRY
-A year-and-a-half later, we were both in Hawaii for the Pro Bowl. Now I've always hit a wall at 18 months. Every serious girlfriend lasts 18 months. It's like --ka-boom. The curse of 18 months.
+DANNY
+Paula?
 
-LYDIA
-That's when you need to cement, and define define define.
+JEFF
+Yeah.
 
-JERRY
-Exactly. And the world does not need another 35 year-old bachelor. I knew I wanted to propose, so I took her there.
+DANNY
+There's nothing wrong between you two?
 
-LYDIA
-To the pools?
+JEFF
+Oh no, nothing like that.  Just that I'm worried, she being there alone and
+those strange things flying over the house and those incidents in the graveyard
+the past few days. It's just got me worried.
 
-JERRY
-To the pools. Now she's Miss Rock Climber, and I'm more the Non-Rock Climber, but we're hiking up through the pools and there's a fine mist in the air, and I have the ring in my pocket, and I'm a little nervous, I'm lagging behind, and she says to me, get this -- "Hurry up, klutz."
+DANNY
+Well, I haven't figured out those crazy skybirds yet but I give you fifty to one
+odds the police have figured out that cemetery thing by now.
 
-Jerry takes a drink of his beer.
+(Enter EDITH)
 
-LYDIA
-Oh no --
+JEFF
+I hope so.
 
-JERRY
-(frowning) Well, it bothered me somewhat. And I got quiet. And now she's quiet and we're both pouting a little, you know. And I decide I'm not going to propose. The mood is not right. Why be impulsive? Now at this point I know she knows that I was going to propose and didn't. And she knows I know. So the entire sixty mile ride back to the airport, we don't speak. And we're both good at that. We fly to Honolulu in silence. We check into the Pro Bowl hotel --
+EDITH
+If you're really that worried Jeff why don't you radio in and find out? Mac
+should be on duty at the field by now. He could call Paula and relay the message
+to you.
 
-LYDIA
-How sad --
-${END_MARKER}
+DANNY
+Hi Edith.
+
+EDITH
+Hi Silents. I haven't heard a word from this end of the plane since we left the
+field.
+
+DANNY
+Jeff's been giving me and himself a study in silence.
+
+EDITH
+You boys are feudin'?
+
+JEFF
+Oh no Edie, nothing like that.
+
+DANNY
+Hey Edie, how about you and me balling it up in Albuquerque?
+
+EDITH
+Albuquerque? Have you read that flight schedule Boy?
+
+DANNY
+What about it?
+
+EDITH
+We land in Albuquerque at 4 am. That's strictly a nine o'clock town.
+
+DANNY
+Well I know a friend that'll help us --
+
+EDITH
+Let's have a problem first, huh Danny.
+
+DANNY
+Ah he's worried about Paula.
+
+EDITH
+I read about that cemetery business. I tried to get you kids to not buy too near
+one of those things. We get there soon enough as it is.
+
+DANNY
+He thought it'd be quiet and peaceful there.
+
+EDITH
+No doubt about that. It's quiet alright, like a tomb. I'm sorry Jeff, that was a
+bad joke.
 
 Example 2.
 `;
